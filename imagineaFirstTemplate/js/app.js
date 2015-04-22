5b7fade18939d407bcae41b176309a62071dba77
Roshan30 <roshan.joshi@imaginea.com>
2015-04-22 17:43:38 +0530

    changes in folder name

--- imagineaFirstTemplate/js/app.js
+++ imagineaFirstTemplate/js/app.js
@@ -10,14 +10,18 @@ var module = angular.module("sampleApp", ['ngRoute']);
  			templateUrl: 'templates/eventDetail.html',
  			controller: 'eventDetailCtrl'
  		}).
+ 		when('/addEvent', {
+ 			templateUrl: 'templates/addEvent.html',
+ 			controller: 'addEventCtrl'
+ 		}).
  		otherwise({
  			redirectTo: '/homePage'
  		});
  	}
  ]);
  module.controller("RouteController", function($scope, $http,$location) {
- 	$http.get("data/path.json").success(function(response) {
- 		$scope.imagePaths = response[0].imagePaths;
+ 	$http.get("data/imagePath.json").success(function(response) {
+ 		$scope.imagePaths = response;
  	});
  	$scope.viewPageFunction = function(data) {
  		if(data == 'grid') {
@@ -52,8 +56,9 @@ var module = angular.module("sampleApp", ['ngRoute']);
  module.controller("eventDetailCtrl", function($scope,$http, $routeParams) {
 
  	
- 		$http.get("data/path.json").success(function(response) {
- 		$.each(response[0].imagePaths, function (key, data) { 	
+ 		$http.get("data/imagePath.json").success(function(response) {
+ 				console.log(response);
+ 		$.each(response, function (key, data) { 	
  		//console.log(data.id+' '+$routeParams.title)				
  					if(data.id==$routeParams.title){
  							
@@ -64,7 +69,48 @@ var module = angular.module("sampleApp", ['ngRoute']);
  	});
 
  });
+ module.controller("addEventCtrl", function($scope,$http) {
+ 		$scope.models={
+ 			event:{
+ 				icon:'css/images/img5.png'
+ 			}
+ 		}
 
+ 		$scope.randomString = function() {
+    		charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   	 		var randomString = '';
+    		for (var i = 0; i < 10; i++) {
+    		var randomPoz = Math.floor(Math.random() * charSet.length);
+    		randomString += charSet.substring(randomPoz,randomPoz+1);
+   	 		}
+   	 		$http.get("data/imagePath.json").success(function(response) {
+ 			$.each(response, function (key, data) { 	
+ 					if (randomString == data.id) {
+ 						$scope.randomString();
+ 					}
+ 			});
+    		});
+			
+			return randomString;
+		}	
+
+
+		$scope.saveEvent = function (){
+			$scope.genratedIdNew = $scope.randomString();
+			$scope.models.event.id = $scope.genratedIdNew;
+				
+						$http.get("data/imagePath.json").success(function(response) {
+ 						response.push($scope.models.event);
+ 						console.log(response);
+    		});
+
+ 					 }
+    		
+
+
+		
+
+ });
 
  safeApply = function($scope, fn) {
  	var phase = $scope.$root.$phase;
