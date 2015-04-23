1ff571730f17c5ccd863ae4d1fca5b5f2d5fc83a
Roshan30 <roshan.joshi@imaginea.com>
2015-04-23 19:37:43 +0530

    changes in all

---
+++ imagineaFirstTemplate/js/controller.js
@@ -0,0 +1,105 @@
+angular.module("sampleApp")
+.controller("RouteController", function($scope, $location,commonService,$commonService) {
+ 
+
+    if(flag.firstTimeEvents){
+        commonService.getEvents().then(function(events){
+      $scope.imagePaths = events;
+      $commonService.setEvents(events);
+    });
+        flag.firstTimeEvents = false;
+    }else{
+        $scope.imagePaths = $commonService.getEvents();
+    }
+    
+
+ 	$scope.viewPageFunction = function(data) {
+ 		if(data == 'grid') {
+ 			$('.containerClass ul').removeClass('list').addClass('grid');
+ 		} else if(data == 'list') {
+ 			$('.containerClass ul').removeClass('grid').addClass('list');
+ 		}
+ 	}
+ 
+
+ 	$scope.navigateToDetails = function (paths) {
+ 		$location.url('eventDetail/' + paths.id);
+ 	}
+    commonService.getSearchData().then(function(data){
+        $scope.searchFieldValGlobal = data;
+   });
+   console.log($scope.searchFieldValGlobal);
+ $scope.$watch('searchFieldValGlobal', function (newVal, oldVal) {
+  
+ });
+
+ })
+
+ .controller("headerCtrl", function($scope, commonService,$commonService) {
+ 	commonService.getePath().then(function(response){
+          $scope.paths = response;
+      });
+      $scope.searchFieldVal={}
+      $scope.updateSearch = function(fieldVal){
+        //console.log(fieldVal);
+       $commonService.setSearchData($scope.searchFieldVal.newVal);
+       searchFieldValGlobal = $scope.searchFieldVal.newVal;
+       //console.log(searchFieldValGlobal);
+      }
+            
+ })
+
+.controller("sideMenuCtrl", function($scope, commonService) {
+ 			commonService.getPath().then(function(response){
+          $scope.paths = response;
+
+      });
+
+ })
+.controller("eventDetailCtrl", function($scope, $routeParams, $commonService) {
+
+    $scope.response = $commonService.getEvents();
+
+ 		$.each($scope.response, function (key, data) { 	
+ 					if(data.id==$routeParams.title){
+ 						$scope.selectedObjet=data;
+ 					}
+ 			});
+ 	
+
+ })
+.controller("addEventCtrl", function($scope,commonService,$location) {
+ 		$scope.models={
+ 			event:{
+ 				icon:'css/images/img5.png'
+ 			}
+ 		}
+
+ 		$scope.randomString = function() {
+    		charSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+   	 		var randomString = '';
+    		for (var i = 0; i < 10; i++) {
+    		var randomPoz = Math.floor(Math.random() * charSet.length);
+    		randomString += charSet.substring(randomPoz,randomPoz+1);
+   	 		}
+   	 		commonService.getEvents().then(function(response){
+ 			    $.each(response, function (key, data) { 	
+ 					if (randomString == data.id) {
+ 						$scope.randomString();
+ 					}
+ 			});
+    		});
+			
+			return randomString;
+		}	
+		$scope.saveEvent = function (){
+		  	$scope.genratedIdNew = $scope.randomString();
+			  $scope.models.event.id = $scope.genratedIdNew;
+         	 jsonData.push($scope.models.event);
+          
+           safeApply($scope, function() {
+            $location.path('#/homePage');
+            });
+          
+ 					 }
+ });
--- imagineaFirstTemplate/js/services.js
+++ imagineaFirstTemplate/js/services.js
@@ -1,12 +1,84 @@
-angular.module("sampleApp", []).
 
-factory('commonService'function($q, $http){
-	var jsonData;
-	var defer = $q.defer();
+var jsonData;
+var flag = {
+	 firstTimeEvents:true
+}
+var searchFieldValGlobal='';
+angular.module("sampleApp").factory('commonService',function($q, $http){
+	
+	
 		function _getEvents (){
-			return defer.promise();
+			var defer = $q.defer();
+				$http.get("data/imagePath.json").success(function(response) {
+ 				jsonData = response;
+ 				defer.resolve(jsonData);
+ 			});
+
+			return defer.promise;
 		}
+		function _getPath (){
+			var defer = $q.defer();
+				$http.get("data/path.json").success(function(response) {
+ 				defer.resolve(response[0].paths);
+ 			});
+
+			return defer.promise;
+		}
+		function _getePath (){
+			var defer = $q.defer();
+				$http.get("data/path.json").success(function(response) {
+ 				defer.resolve(response[0].epaths);
+ 			});
+
+			return defer.promise;
+		}
+		var factory={}
+		function _getSearchData (){
+			console.log('factory'+searchFieldValGlobal);
+			var defer = $q.defer();
+			defer.resolve(searchFieldValGlobal);
+			return defer.promise;
+		}
+
 	return{
-		getEvents:_getEvents()
+		getEvents:_getEvents,
+		getPath:_getPath,
+		getePath:_getePath,
+		getSearchData:_getSearchData
 	}
-});
+
+
+}).service('$commonService', function () {
+       var events;
+       var searchData;
+	  return {
+            getEvents: function () {
+                return events;
+            },
+            setEvents: function(value) {
+                events = value;
+            },
+            getSearchData:function(){
+            	return searchData;
+            },
+            setSearchData:function(data){
+            	searchData = data;
+            }
+            }
+        });
+
+
+
+
+
+
+ safeApply = function($scope,fn) {
+  var phase = $scope.$root.$$phase;
+  if(phase == '$apply' || phase == '$digest') {
+    if(fn && (typeof(fn) === 'function')) {
+      fn();
+    }
+  } else {
+    $scope.$apply(fn);
+  }
+};
